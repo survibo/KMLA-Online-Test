2be3a6a26e1a43cae4838e42264d4bbf0efd004c
survibo <survibo@gmail.com>
2026-03-25 23:52:10 +0900

    .

--- src/Example.jsx
+++
@@ -1,6 +0,0 @@
-import { MainFooter } from "@/blocks/main/footer"
-import { activeMainFooterScenario } from "@/blocks/main/footer/mock.scenarios"
-
-export function Example() {
-  return <MainFooter data={activeMainFooterScenario.data} />
-}
